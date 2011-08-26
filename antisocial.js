199a9f0bd0a8bc83d0bca6e8b7cd0edc0947b8a1
Norm Yee <normanyee123@gmail.com>
2011-08-25 17:22:59 -0700

    encapsulate main logic in a self-invoking function

--- antisocial.js
+++ antisocial.js
@@ -1,30 +1,32 @@
-var blocklist = [
-	"apis.google.com",
-	"plusone.google.com",
-	"ssl.gstatic.com",
-	"connect.facebook.net",
-	"www.facebook.com",
-	"static.ak.fbcdn.net",
-	"meebo.com",
-	"s.meebocdn.net",
-	"platform.twitter.com",
-	"widgets.twimg.com",
-	"s7.addthis.com",
-	"platform.linkedin.com",
-	"www.stumbleupon.com",
-	"cdn.wibiya.com",
-	"w.sharethis.com",
-	"tweetmeme.com",
-	"www.reddit.com",
-	"widgets.digg.com"
-]
-
-var re = /https?:\/\/([^\/]+)/i;
-
-document.addEventListener("beforeload", function(event) {
-	var result =  re.exec(event.url);
-
-	if (result && (blocklist.indexOf(result[1]) > -1)) {
-		event.preventDefault();
-	}
-}, true);
+(function() {
+	var blocklist = [
+		"apis.google.com",
+		"plusone.google.com",
+		"ssl.gstatic.com",
+		"connect.facebook.net",
+		"www.facebook.com",
+		"static.ak.fbcdn.net",
+		"meebo.com",
+		"s.meebocdn.net",
+		"platform.twitter.com",
+		"widgets.twimg.com",
+		"s7.addthis.com",
+		"platform.linkedin.com",
+		"www.stumbleupon.com",
+		"cdn.wibiya.com",
+		"w.sharethis.com",
+		"tweetmeme.com",
+		"www.reddit.com",
+		"widgets.digg.com"
+	]
+	
+	var re = /https?:\/\/([^\/]+)/i;
+	
+	document.addEventListener("beforeload", function(event) {
+		var result = re.exec(event.url);
+	
+		if (result && (blocklist.indexOf(result[1]) > -1)) {
+			event.preventDefault();
+		}
+	}, true);
+}());
